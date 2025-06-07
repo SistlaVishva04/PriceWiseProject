08a98a9b0167281e9611b911d6825d6f0816f018
SistlaVishva2004 <vishnuvamsi04@gmail.com>
2025-06-07 17:24:02 +0530

    changee

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                     // Root-relative paths
+  base: '/',
   build: {
-    outDir: 'dist',              // Must match netlify.toml's publish
-    emptyOutDir: true,           // Cleans dist folder before build
+    outDir: 'dist', 
+    emptyOutDir: true, 
   },
   server: {
-    port: 5173,                  // Must match netlify.toml
-    strictPort: true             // Prevent automatic port switching
+    port: 5173, 
+    strictPort: true 
   }
 })
